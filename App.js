00b73e018a331b99f163593f0178c3284fb26fae
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-11-23 11:20:43 +0300

    layout with flexbox: width and height

--- App.js
+++ App.js
@@ -1,20 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+} from "react-native";
 
 export default function App() {
+  const [widthType, setWidthType] = useState("auto");
+  const [heightType, setHeightType] = useState("auto");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PreviewLayout
+      widthType={widthType}
+      heightType={heightType}
+      widthValues={["auto", 300, "80%"]}
+      heightValues={["auto", 200, "60%"]}
+      setWidthType={setWidthType}
+      setHeightType={setHeightType}
+    >
+      <View
+        style={{
+          alignSelf: "flex-start",
+          backgroundColor: "aliceblue",
+          height: heightType,
+          width: widthType,
+          padding: 15,
+        }}
+      >
+        <View style={[styles.box, { backgroundColor: "powderblue" }]} />
+        <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+        <View style={[styles.box, { backgroundColor: "steelblue" }]} />
+      </View>
+    </PreviewLayout>
   );
 }
 
+const PreviewLayout = ({
+  children,
+  widthType,
+  heightType,
+  widthValues,
+  heightValues,
+  setWidthType,
+  setHeightType,
+}) => (
+  <SafeAreaView style={{ flex: 1, padding: 10 }}>
+    <View style={styles.row}>
+      <Text style={styles.label}>width</Text>
+      {widthValues.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setWidthType(value)}
+          style={[styles.button, widthType === value && styles.selected]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              widthType === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.label}>height</Text>
+      {heightValues.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setHeightType(value)}
+          style={[styles.button, heightType === value && styles.selected]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              heightType === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    {children}
+  </SafeAreaView>
+);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    alignSelf: "flex-start",
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  selected: {
+    backgroundColor: "coral",
+    shadowOpacity: 0,
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "coral",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
